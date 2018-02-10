0c6add7453055021475b056f6f0c63b7f84aa292
colinxfleming <c3flemin@gmail.com>
2018-02-09 19:18:44 -0500

    Add working compiler

--- compile_seasons.py
+++ compile_seasons.py
@@ -1,11 +1,47 @@
+import json
+import os
 import sys
 
-output_format = sys.argv[1]
+import yaml
 
-if not output_format:
-    raise('Please set an output format: yml json')
+error_message = 'Please set an output format: yml json'
+
+basedir = os.path.dirname(os.path.realpath(__file__))
+
+try:
+    output_format = sys.argv[1]
+    list(['yml', 'json']).index(output_format)
+    if not output_format:
+        print(error_message)
+        raise
+except ValueError as e:
+    print(error_message)
+    raise(e)
+except IndexError as e:
+    print(error_message)
+    raise(e)
+
+all_data = {'episodes': [], 'characters': []}
+
+seasons = os.listdir(os.path.join(basedir, 'episodes'))
+for season in seasons:
+    content = yaml.load(
+        open(os.path.join(basedir, 'episodes', season)))
+    all_data['episodes'] = all_data['episodes'] + content['episodes']
+
+all_data['episodes'].sort(key=lambda x: (x['season'], x['episode']))
+
+if output_format == 'yml':
+    raise ValueError('not yet, sorry. just json for now')
+    # output = yaml.dump(all_data)
+elif output_format == 'json':
+    output = json.dumps(all_data)
+else:
+    raise ValueError('????')
 
 
+output_file = 'simpsons_data.{}'.format(output_format)
+with open(os.path.join(basedir, output_file), 'w') as f:
+    f.write(output)
 
-
-
+print('Compilation complete in {}'.format(output_format))
