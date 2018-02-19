1d722e28217e1087f3206b61808210c886215c32
colinxfleming <c3flemin@gmail.com>
2018-02-18 23:11:31 -0500

    all json everything for now

--- compile_seasons.py
+++ compile_seasons.py
@@ -1,25 +1,26 @@
 import json
 import os
-import sys
+# import sys
 
 import yaml
 
-error_message = 'Please set an output format: yml json'
+# error_message = 'Please set an output format: yml json'
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 
-try:
-    output_format = sys.argv[1] if len(sys.argv) > 2 else 'json'
-    list(['yml', 'json']).index(output_format)
-    if not output_format:
-        print(error_message)
-        raise
-except ValueError as e:
-    print(error_message)
-    raise(e)
-except IndexError as e:
-    print(error_message)
-    raise(e)
+output_format = 'json'  # hardsetting fornow
+# try:
+#     output_format = sys.argv[1] if len(sys.argv) > 2 else 'json'
+#     list(['yml', 'json']).index(output_format)
+#     if not output_format:
+#         print(error_message)
+#         raise
+# except ValueError as e:
+#     print(error_message)
+#     raise(e)
+# except IndexError as e:
+#     print(error_message)
+#     raise(e)
 
 all_data = {'episodes': [], 'characters': []}
 
