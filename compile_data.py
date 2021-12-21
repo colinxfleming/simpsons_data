12b41a7908e08358386b75a700e2254139733b7b
colinxfleming <c3flemin@gmail.com>
2021-12-21 11:37:20 -0500

    fmt + githubci

--- compile_data.py
+++ compile_data.py
@@ -7,30 +7,29 @@ import yaml
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 
-all_data = {'episodes': []}
+all_data = {"episodes": []}
 
 for datatype in all_data.keys():
     data_files = os.listdir(os.path.join(basedir, datatype))
     for f in data_files:
-        content = yaml.load(
-            open(os.path.join(basedir, datatype, f)))
+        content = yaml.load(open(os.path.join(basedir, datatype, f)))
         all_data[datatype] = all_data[datatype] + content[datatype]
 
 # Individual sorting rules
-all_data['episodes'].sort(key=lambda x: (x['season'], x['episode']))
+all_data["episodes"].sort(key=lambda x: (x["season"], x["episode"]))
 
-for index, ep in enumerate(all_data['episodes']):
+for index, ep in enumerate(all_data["episodes"]):
     # Turn all 'maybe' good episodes to bad episodes
-    if all_data['episodes'][index]['good'] == 'maybe':
-        all_data['episodes'][index]['good'] = False
+    if all_data["episodes"][index]["good"] == "maybe":
+        all_data["episodes"][index]["good"] = False
 
     # Turn all empty guest star arrays to [] from nil
-    if all_data['episodes'][index]['guest_stars'] is None:
-        all_data['episodes'][index]['guest_stars'] = []
+    if all_data["episodes"][index]["guest_stars"] is None:
+        all_data["episodes"][index]["guest_stars"] = []
 
 output = json.dumps(all_data, indent=4)
-output_file = 'simpsons_data.json'
-with open(os.path.join(basedir, output_file), 'w') as f:
+output_file = "simpsons_data.json"
+with open(os.path.join(basedir, output_file), "w") as f:
     f.write(output)
 
-print('Compilation complete - simpsons_data.json')
+print("Compilation complete - simpsons_data.json")
--- test_build.py
+++ test_build.py
@@ -3,20 +3,26 @@
 """A series of asserts to verify data quality."""
 import json
 
-expected_ep_keys = ['title', 'season', 'episode', 'description', 'disneyplus_id',
-                    'simpsonsworld_id', 'good', 'characters']
-expected_char_keys = ['name', 'short_name']
+expected_ep_keys = [
+    "title",
+    "season",
+    "episode",
+    "description",
+    "disneyplus_id",
+    "simpsonsworld_id",
+    "good",
+    "guest_stars",
+]
 
-print('loading file...')
+print("loading file...")
 
-with open('simpsons_data.json', 'r') as f:
+with open("simpsons_data.json", "r") as f:
     all_json = json.load(f)
-    all_episodes = all_json['episodes']
-    all_characters = all_json['characters']
+    all_episodes = all_json["episodes"]
 
     # EPISODE QA #
 
-    print('QAing episodes...')
+    print("QAing episodes...")
 
     for episode in all_episodes:
         # Should have expected keys
@@ -26,69 +32,39 @@ with open('simpsons_data.json', 'r') as f:
         assert len(expected_ep_keys) == len(episode.keys())
 
         # Title should be a string
-        assert isinstance(episode['title'], str)
-        assert len(episode['title']) > 0
+        assert isinstance(episode["title"], str)
+        assert len(episode["title"]) > 0
 
         # Season should be an int
-        assert isinstance(episode['season'], int)
+        assert isinstance(episode["season"], int)
         # assert episode['season'] < 50
 
         # Episode should be an int
-        assert isinstance(episode['episode'], int)
+        assert isinstance(episode["episode"], int)
         # assert episode['episode'] < 50
 
         # Description should be a string
-        assert isinstance(episode['description'], str)
+        assert isinstance(episode["description"], str)
 
         # Simpsonsworld ID should be a str if set
-        if episode['disneyplus_id']:
-            assert isinstance(episode['disneyplus_id'], str)
+        if episode["disneyplus_id"]:
+            assert isinstance(episode["disneyplus_id"], str)
 
         # Simpsonsworld ID should be an int if set
-        if episode['simpsonsworld_id']:
-            assert isinstance(episode['simpsonsworld_id'], int)
+        if episode["simpsonsworld_id"]:
+            assert isinstance(episode["simpsonsworld_id"], int)
 
         # Good should be a boolean
-        assert isinstance(episode['good'], bool)
+        assert isinstance(episode["good"], bool)
 
         # Characters should be an array of strings
-        assert isinstance(episode['characters'], list)
-        for character in episode['characters']:
+        assert isinstance(episode["guest_stars"], list)
+        for character in episode["guest_stars"]:
             assert isinstance(character, str)
 
-            # Every character should exist in the corresponding characters blob
-            present = [char for char
-                       in all_characters
-                       if char['short_name'] == character]
-            assert present, f"No {character} in character yml"
-
-
     # Season-Episode pair should be unique
-    assert len(all_episodes) == len(set(f"{ep['season']}-{ep['episode']}"
-                                        for ep in all_episodes))
+    assert len(all_episodes) == len(
+        set(f"{ep['season']}-{ep['episode']}" for ep in all_episodes)
+    )
 
-    # CHARACTER QA #
-
-    print('QAing characters...')
-
-    for character in all_characters:
-        # Should have expected keys
-        for key in expected_char_keys:
-            assert key in character
-        assert len(expected_char_keys) == len(character.keys())
-
-        # Name should be a string
-        assert isinstance(character['name'], str)
-
-        # Short name should be a string
-        assert isinstance(character['short_name'], str)
-
-    # Character name should be unique
-    assert len(all_characters) == len(set(f"#{char['name']}"
-                                          for char in all_characters))
-
-    # Character shortname should be unique
-    assert len(all_characters) == len(set(f"#{char['short_name']}"
-                                          for char in all_characters))
-
-print('QA complete!')
+print("QA complete!")
