101a0a8867bcd15aec420dbc3ab00f9df95bd81e
Colin <c3flemin@gmail.com>
2021-12-21 11:55:21 -0500

    fmt + githubci (#47)
    * fmt + githubci
    
    * recompile
    
    * rm circleci
    
    * adjust remaining report
    
    * build pass
    
    * put ci in right spot
    
    * adjust test cmd

--- remaining_report.py
+++ remaining_report.py
@@ -8,24 +8,22 @@ import yaml
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 
-all_data = {'episodes': [], 'characters': []}
+all_data = {"episodes": []}
 
 for datatype in all_data.keys():
     data_files = os.listdir(os.path.join(basedir, datatype))
     for f in data_files:
-        content = yaml.load(
-            open(os.path.join(basedir, datatype, f)))
+        content = yaml.load(open(os.path.join(basedir, datatype, f)))
         all_data[datatype] = all_data[datatype] + content[datatype]
 
-all_data['episodes'].sort(key=lambda x: (x['season'], x['episode']))
-all_data['characters'].sort(key=lambda x: (x['short_name']))
+all_data["episodes"].sort(key=lambda x: (x["season"], x["episode"]))
 
-print('***EPISODE GOODNESS/BADNESS TO REVIEW***\n')
-for ep in all_data['episodes']:
-    if isinstance(ep['good'], str):
+print("***EPISODE GOODNESS/BADNESS TO REVIEW***\n")
+for ep in all_data["episodes"]:
+    if isinstance(ep["good"], str):
         print(f"{ep['season']}-{ep['episode']} - {ep['title']}")
 
-print('\nEPISODES TO LOG CHARACTERS')
-for ep in all_data['episodes']:
-    if ep['characters'] is None:
+print("\nEPISODES TO LOG GUEST STARS")
+for ep in all_data["episodes"]:
+    if not ep["guest_stars"]:
         print(f"{ep['season']}-{ep['episode']} - {ep['title']}")
